093347e8c62e3c64584108d39d6ad9bd72656d23
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-12 20:18:45 +0000

    Added in getVenue and getVenues along with tests

--- code/server_software/prototype/googlePlaces.rb
+++ code/server_software/prototype/googlePlaces.rb
@@ -16,4 +16,14 @@ class GooglePlaces < Resource
 		return@base_uri;
 	end
 
+	def getVenue(venueID)
+		@url = "#{@base_uri}/details/json?key=#{@api_key}&sensor=false&reference=#{venueID}";
+		return self.getResource(@url);
+	end
+
+	def getVenues()
+		@url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.41649271,-4.07785633&radius=500&sensor=false&key=AIzaSyDvq9AffhVGnPL6byYitdUB54gxlPtTCgg";
+		return self.getResource(@url);
+	end
+
 end
--- code/server_software/prototype/spec/googlePlaces_spec.rb
+++ code/server_software/prototype/spec/googlePlaces_spec.rb
@@ -20,4 +20,18 @@ describe GooglePlaces do
 		expect(@googleplaces.getBaseURI).to eq("https://maps.googleapis.com/maps/api/place");
 	end
 
+	it ".getVenue" do
+		venue = File.read(File.dirname(__FILE__) + '/fixtures/googleplaces/venue.json');
+		@venueJSON = JSON.parse(venue);
+		  stub_request(:get, "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAsU5XpnhyRwiuxqSHxtJnmFJ9nAYsq-Kk&reference=328e4dc5e8f41286bddd70df00d4e56ec76facc3&sensor=false").to_return(:body => @venueJSON);
+		expect(@googleplaces.getVenue("328e4dc5e8f41286bddd70df00d4e56ec76facc3")).to eq(@venueJSON);
+	end
+
+	it ".getVenues" do
+		venues = File.read(File.dirname(__FILE__) + '/fixtures/googleplaces/venues.json');
+		@venuesJSON = JSON.parse(venues);
+		stub_request(:get, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.41649271,-4.07785633&radius=500&sensor=false&key=AIzaSyDvq9AffhVGnPL6byYitdUB54gxlPtTCgg").to_return(:body => @venuesJSON);
+
+		expect(@googleplaces.getVenues).to eql(@venuesJSON);
+	end
 end
