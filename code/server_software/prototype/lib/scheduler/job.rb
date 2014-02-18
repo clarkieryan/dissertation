a096bef53f8d9fb3b3e9e924ae0284ccf8d6648d
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-18 21:44:11 +0000

    Removed ToDO

--- code/server_software/prototype/lib/scheduler/job.rb
+++ code/server_software/prototype/lib/scheduler/job.rb
@@ -15,9 +15,6 @@ class Job
 
 	def startJob 
 		require_relative '../resources/'+@api;
-		#TODO shouldnt have to pass in app stuff here 
-		# Possibley pulled from a database
-		#Dynamically load the class
 		return eval(@api.capitalize).new;
 	end
 	#Could add in helper functions getValidFilters etc. 
