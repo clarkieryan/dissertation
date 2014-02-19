36a882bbe62204ce518d3897e475e7fb29f5f712
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-19 17:10:25 +0000

    Added in some basic authentication

--- code/server_software/tonight_server/app/controllers/admin/static_controller.rb
+++ code/server_software/tonight_server/app/controllers/admin/static_controller.rb
@@ -2,6 +2,9 @@ class Admin::StaticController < ApplicationController
 	#Set the layout to be admin
 	layout 'admin'
 
+	#Ensure they are authorised - Can be replaced with proper auth
+	before_filter :admin_required;
+
 	def index 
 		
 	end
