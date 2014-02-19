eed5d0cd68e736203d66bc691e67edc52fa70cd9
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-19 17:56:29 +0000

    Added in tests and updated some tests

--- code/server_software/tonight_server/spec/controllers/admin/static_controller_spec.rb
+++ code/server_software/tonight_server/spec/controllers/admin/static_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe StaticController do
+describe Admin::StaticController do
 
 	describe "index" do
 		it "renders the index template" do
@@ -9,11 +9,13 @@ describe StaticController do
 			expect(response.body).to eq ""
 		end
 
-		it "renders the static/index template" do
+		it "renders the admin/index template" do
 			get :index
-			expect(response).to render_template("static/index")
+			expect(response).to render_template("admin/static/index")
 			expect(response.body).to eq ""
 		end
  	 end
- 	 	
+
+ 	 
+
 end
