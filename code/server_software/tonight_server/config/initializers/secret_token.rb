23b30906831798a1603151f2a36f5ad2854550c1
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-19 09:59:47 +0000

    Added in the base rails code for the server element to run off

---
+++ code/server_software/tonight_server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TonightServer::Application.config.secret_key_base = '33312a72186436af80ff746a3e47d0f20cd5cebd4cbfa4cb4a9608f4b6033630f7cf5bcfe5bc45d3356e4cd3527b0261cd0ab6207c7ae3c6fecacd19babf0c72'
