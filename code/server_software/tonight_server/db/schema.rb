928538ff1cb51997e1085237c241a67645cb3586
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-23 22:34:46 +0000

    Updated test database to use postgres, for faster tests (Postgres supports concurrency)

--- code/server_software/tonight_server/db/schema.rb
+++ code/server_software/tonight_server/db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220003918) do
+ActiveRecord::Schema.define(version: 20140223192030) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "jobs", force: true do |t|
     t.string   "name"
@@ -20,6 +23,15 @@ ActiveRecord::Schema.define(version: 20140220003918) do
     t.string   "filters"
     t.string   "toe"
     t.string   "output"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
