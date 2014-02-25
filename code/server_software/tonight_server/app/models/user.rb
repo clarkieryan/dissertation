3d4348fdce6c0fd4b7c465632aaca9833e6a1f13
Ryan Clarke <clarkie.ryan@gmail.com>
2014-02-25 16:52:49 +0000

    Added in user model validation

--- code/server_software/tonight_server/app/models/user.rb
+++ code/server_software/tonight_server/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
 end
+# t.string :first_name
+# t.string :last_name
+# t.string :password
+# t.string :email
--- code/server_software/tonight_server/spec/models/user_spec.rb
+++ code/server_software/tonight_server/spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  	it 'is invalid without a first name' do
+  		expect(FactoryGirl.build(:user, :first_name => nil)).to_not be_valid;
+  	end
+
+  	it 'is invalid without a last name' do
+  		expect(FactoryGirl.build(:user, :last_name => nil)).to_not be_valid;
+  	end
+
+  	it ' is invalid without a password' do
+  		expect(FactoryGirl.build(:user, :password => nil)).to_not be_valid;
+  	end
+
+  	it "is invalid with an email" do
+  	  	expect(FactoryGirl.build(:user, :email => nil)).to_not be_valid;
+  	end
+  	
 end
