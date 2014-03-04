0e549018557fcfa944beeb72942f6e8dc401862d
Ryan Clarke <clarkie.ryan@gmail.com>
2014-03-04 22:19:45 +0000

    Updated migrations

--- code/server_software/tonight_server/db/migrate/20140228183704_create_events.rb
+++ code/server_software/tonight_server/db/migrate/20140228183704_create_events.rb
@@ -4,13 +4,13 @@ class CreateEvents < ActiveRecord::Migration
       t.integer :ref_id, :limit => 8
       t.integer :venue_id
       t.string :name
-      t.string :desc
+      t.text :desc
       t.integer :start_time
       t.integer :end_time
       t.string :timezone
       t.integer :venue
       t.integer :updated_at
-      
+
       t.timestamps
     end
   end
