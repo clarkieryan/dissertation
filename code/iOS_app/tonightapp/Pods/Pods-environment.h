0a261e8a1285882a2964285a758e149f7fc24c32
Ryan Clarke <clarkie.ryan@gmail.com>
2014-03-08 01:13:47 +0000

    Adding in a number of dependancies

---
+++ code/iOS_app/tonightapp/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// UICKeyChainStore
+#define COCOAPODS_POD_AVAILABLE_UICKeyChainStore
+#define COCOAPODS_VERSION_MAJOR_UICKeyChainStore 1
+#define COCOAPODS_VERSION_MINOR_UICKeyChainStore 0
+#define COCOAPODS_VERSION_PATCH_UICKeyChainStore 4
+
+// Unirest
+#define COCOAPODS_POD_AVAILABLE_Unirest
+#define COCOAPODS_VERSION_MAJOR_Unirest 1
+#define COCOAPODS_VERSION_MINOR_Unirest 1
+#define COCOAPODS_VERSION_PATCH_Unirest 2
+
