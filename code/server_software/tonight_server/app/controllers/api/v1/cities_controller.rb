d9edb803dcce6bc60d2e14c2a8a77f17225a4148
Ryan Clarke <clarkie.ryan@gmail.com>
2014-03-10 20:48:30 +0000

    Added in the cities controller

---
+++ code/server_software/tonight_server/app/controllers/api/v1/cities_controller.rb
@@ -0,0 +1,10 @@
+class API::V1::CitiesController < ApplicationController
+
+	#doorkeeper_for :all
+
+	def index 
+		cities = City.order('name ASC');
+		render json: cities;
+	end
+
+end
---
+++ code/server_software/tonight_server/spec/controllers/api/v1/cities_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe API::V1::CitiesController do
+  	
+	let (:user) {FactoryGirl.create(:user)}
+	let(:token) { double :accessible? => true, :resource_owner_id => user.id }
+
+	before do	
+	 	controller.stub(:doorkeeper_token) { token }
+	end
+
+	describe 'index' do
+		
+		it 'should return a 200' do
+			get :index;
+			expect(response.code).to eq("200");
+		end
+
+
+	end
+
+end
