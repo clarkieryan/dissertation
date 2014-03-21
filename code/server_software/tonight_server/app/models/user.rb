553ea662e65ae7a746dc4fbce93a9880dbacdc3e
Ryan Clarke <clarkie.ryan@gmail.com>
2014-03-21 13:44:18 +0000

    Added in following component and some changes to the user model

--- code/server_software/tonight_server/app/models/user.rb
+++ code/server_software/tonight_server/app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
 	validates :password, presence: true
 	validates_length_of :password, :in => 6..20, :on => :create
 
+	has_many :events
+
 	def name 
 		return "#{first_name} #{last_name}"
 	end
